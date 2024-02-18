16270e2e84ffa9ec39bd1d703a6190caa5ab45f1
mayaradepaula1997 <mayara.paula1997@gmail.com>
2024-02-18 11:47:30 -0300

    Order, Instant, PARTE 1

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -4,11 +4,14 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
+
     @Serial
     private static final Long serialVersionUID = 1L;
 
@@ -20,6 +23,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany (mappedBy = "client") // nome do atributo que está do outro lado da associação na classe "ORDER"
+    private List <Order> orders = new ArrayList<>();
 
     public User(){
 
@@ -73,6 +79,9 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public List <Order> getOrders () {
+        return orders;
+    }
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
