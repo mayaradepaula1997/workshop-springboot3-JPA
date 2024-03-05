ad400597b205934486a444e346f220bb1a187be5
mayaradepaula1997 <mayara.paula1997@gmail.com>
2024-03-04 23:09:55 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -82,6 +82,10 @@ public class TestConfig implements CommandLineRunner { //Para inserir os dados n
         //ASSOCIAÇÕES ENTRE OBJETOS (MODELO RELACIONAL)
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 
+        Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+        o1.setPayment(pay1);
+        orderRepository.save(o1);
+
         OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
         OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
         OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
