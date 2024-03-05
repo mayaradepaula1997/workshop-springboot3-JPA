d7456d818174f28e007510db812b15872945e2a7
mayaradepaula1997 <mayara.paula1997@gmail.com>
2024-03-05 07:59:58 -0300

    SubTotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -91,6 +91,15 @@ public class Order implements Serializable {
         return items;
     }
 
+    public Double getTotal(){
+        double soma = 0.0;
+        for (OrderItem x : items){
+            soma = soma + x.getSubTotal();
+        }
+        return soma;
+    }
+
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
