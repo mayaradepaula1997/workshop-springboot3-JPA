5dbda6212f4f1dd9a2d7fb6b60ffe3b731779343
mayaradepaula1997 <mayara.paula1997@gmail.com>
2024-03-11 07:43:56 -0300

    Exception handling Delete and Update

--- src/main/java/com/educandoweb/course/service/UserService.java
+++ src/main/java/com/educandoweb/course/service/UserService.java
@@ -3,6 +3,7 @@ package com.educandoweb.course.service;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
 import com.educandoweb.course.service.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,13 +27,22 @@ public class UserService {
     public User insert (User obj){
         return repository.save(obj);
     }
-    public void delete (Long id){
-        repository.deleteById(id); //deletar o usuario que tiver esse id
+    public void delete(Long id){
+        try {
+            if(!repository.existsById(id)) throw new ResourceNotFoundException(id);
+            repository.deleteById(id);
+        }catch (ResourceNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
-    public User update (Long id, User obj){   //User: retorna o usuario atualizado
-        User entity = repository.getReferenceById(id);   //repository.getReferenceById instancia o usuario, sendo que não vai no banco de dados
-        updateData (entity, obj);   //pegar esse objeto "entity" atualizar e atualizar o objeto "obj" com os dados que vieram do "(User obj)"
-        return repository.save(entity);
+    public User update (Long id, User obj) {   //User: retorna o usuario atualizado
+        try {
+            User entity = repository.getReferenceById(id);   //repository.getReferenceById instancia o usuario, sendo que não vai no banco de dados
+            updateData(entity, obj);   //pegar esse objeto "entity" atualizar e atualizar o objeto "obj" com os dados que vieram do "(User obj)"
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) { //implementar o "updateData", atualizar os dados do "entity" com base no que chegou no "obj"
